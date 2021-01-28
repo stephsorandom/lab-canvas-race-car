8f909e8edba86bce7a45afd4d0b555e87f761bd9
Stephanie Mitchell <stephsorandom@gmail.com>
2021-01-28 13:01:04 -0500

    iteration4

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 let canvas = document.querySelector("canvas")
 
-
 canvas.width = 500
 canvas.height = 700
 
 let ctx = canvas.getContext("2d")
 
+//ITERATION 1
 let roadImg = new Image()
 roadImg.src = "./images/road.png"
 // let road = {x:0, y:0, w:450, h:600, img:roadImg}
@@ -13,12 +13,18 @@ roadImg.src = "./images/road.png"
 //   ctx.drawImage(roadImg, 0, 0, 450, 600)
 // }
 
+//ITERATION 2 
 let carImg = new Image()
 carImg.src = "./images/car.png"
 // carImg.onload = function() {
 //   ford.draw()
 // }
 
+
+//ITERATION 3
+
+//creating this class is a blue print
+//gives paramaters to blueprint 
 class Car {
   constructor() {
     this.x = 250
@@ -30,6 +36,7 @@ class Car {
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
   }
+  //this goes with CONTROL KEYS and tells blueprint how far to move in each direction when key is pressed on.
     moveUp() {
       this.y -= 30
     }
@@ -44,12 +51,13 @@ class Car {
     }
 }
 
+//creating a new car, takes the blueprint from ^^ and prints that new car
 let ford = new Car()
 ford.draw()
 
 
 
-
+//redraws canvas on every animation pixel
 let interval = null
 function animate() {
   interval = requestAnimationFrame(animate)
@@ -57,16 +65,17 @@ function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
 
-
+//lets it know every move the car makes, to redraw the screen
   ctx.drawImage(roadImg, 0, 0, 450, 600)
+  //calls function to draw
   ford.draw()
 
 }
 animate()
 
 
-
 // CONTROL KEYS 
+//onkeydown function : lets computer know when keys are pushed down, to move in that direction
 window.onkeydown = function(e) {
   switch(e.key) {
     case "ArrowLeft":
@@ -83,6 +92,10 @@ window.onkeydown = function(e) {
      break;
   }
 }
+
+
+//ITERATION 4
+
 
 
 
